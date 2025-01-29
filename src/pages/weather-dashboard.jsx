f0c18f10460b0cfa6668f9fd8950c25e48f45543
Hakim Waithaka <hakim.waithaka@gmail.com>
2025-01-29 19:21:21 +0300

    commit

--- src/pages/weather-dashboard.jsx
+++ src/pages/weather-dashboard.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable no-unused-vars */
+import {CurrentWeather} from "@/components/current-weather";
+import HourlyTemperature from "@/components/hourly-temp";
 import WeatherSkeleton from "@/components/loading-skeleton";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
+import WeatherForecast from "@/components/weather-forecast";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import { useForecastQuery, useReverseGeocodeQuery, useWeatherQuery } from "@/hooks/use-weather";
 import { AlertTriangle, MapPin, RefreshCw } from "lucide-react";
@@ -14,19 +17,20 @@ const WeatherDashboard = () => {
      } = useGeolocation();
 
 
-     const weatherQuery = useWeatherQuery(coordinates)
-     const forecastQuery = useForecastQuery(coordinates)
-     const locationQuery = useReverseGeocodeQuery(coordinates)
-  
+     const weatherQuery = useWeatherQuery(coordinates) || {};
+     const forecastQuery = useForecastQuery(coordinates) || {};
+     const locationQuery = useReverseGeocodeQuery(coordinates) || {};
 
-  const handleRefresh=()=>{
-       getLocation();
-       if (coordinates) {
-         weatherQuery.refetch();
-         forecastQuery.refetch();
-         locationQuery.refetch();
-       }
-  };
+
+     const handleRefresh = () => {
+      getLocation();
+      if (coordinates?.latitude && coordinates?.longitude) {
+        weatherQuery.refetch();
+        forecastQuery.refetch();
+        locationQuery.refetch();
+      }
+    };
+    
 
   if (locationLoading) {
     return <WeatherSkeleton/>
@@ -80,9 +84,10 @@ const WeatherDashboard = () => {
   </Alert>
 )}
 
-if(!weatherQuery.data || forecastQuery.data) {
-  return <WeatherSkeleton/>
+if (!weatherQuery.data || !forecastQuery.data) {
+  return <WeatherSkeleton />;
 }
+
 
   return (
     <div className="space y-4">
@@ -92,13 +97,30 @@ if(!weatherQuery.data || forecastQuery.data) {
             <Button variant={'outline'}
             size={"icon"}
             onClick={handleRefresh}
-            disabled ={weatherQuery.Query.isFetching || forecastQuery.isFetching}
+            disabled={weatherQuery?.isFetching || forecastQuery?.isFetching}
+
              >
                 <RefreshCw className="h-4 w-4"/>
             </Button>
         </div>
 
-        {/* current and Hourly Weather */}
+        
+        <div className="grid gap-6">
+          <div className="flex flex-col lg:flex-row gap-4">
+            <CurrentWeather 
+            data={weatherQuery.data} 
+            locationName={locationName}
+            />
+            <HourlyTemperature
+            data={forecastQuery.data}/>
+         
+          </div>
+
+          <div>
+          <WeatherForecast data={forecastQuery.data} />
+            {/* forecast */}
+          </div>
+        </div>
     </div>
   )
 }
