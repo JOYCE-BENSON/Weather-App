ab08b80fb66cca0aaba49de0b33b343ef3267dfd
Hakim Waithaka <hakim.waithaka@gmail.com>
2025-01-30 13:09:50 +0300

    commit

--- src/pages/city-page.jsx
+++ src/pages/city-page.jsx
@@ -1,7 +1,73 @@
+import CurrentWeather from "@/components/current-weather";
+import HourlyTemperature from "@/components/hourly-temp";
+import WeatherSkeleton from "@/components/loading-skeleton";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import WeatherForecast from "@/components/weather-forecast";
+import { useForecastQuery, useWeatherQuery } from "@/hooks/use-weather";
+import { AlertTriangle,} from "lucide-react";
+import { useSearchParams } from "react-router-dom";
+
+
 const CityPage = () => {
-  return (
-    <div>CityPage</div>
+    const [searchParams] = useSearchParams()
+    const params = useParams();
+    const lat = parseFloat(searchParams.get("lat") || "0");
+    const lon = parseFloat(searchParams.get("lon") || "0");
+
+    const coordinates = {lat,lon};
+
+    const weatherQuery = useWeatherQuery(coordinates);
+    const forecastQuery = useForecastQuery(coordinates);
+
+    if (weatherQuery.error || forecastQuery.error) {
+        return (
+          <Alert variant="destructive">
+            <AlertTriangle className="h-4 w-4" />
+            <AlertDescription>
+              Failed to load weather data. Please try again.
+            </AlertDescription>
+          </Alert>
+        );
+      }
+
+
+      if (!weatherQuery.data || !forecastQuery.data || !params.cityName) {
+        return <WeatherSkeleton />;
+      }
+
+   return (
+    <div className="space y-4">
+        {/* favourite cities */}
+        <div className="flex items-center justify-between">
+            <h1 className="text-3xl font-bold tracking-tight">
+                {params.cityName}, {weatherQuery.data.sys.countery}
+                </h1>
+                <div>
+                    {/* fav */}
+                </div>
+            
+        </div>
+
+        
+        <div className="grid gap-6">
+          <div className="flex flex-col gap-4">
+            <CurrentWeather 
+            data={weatherQuery.data} 
+            
+            />
+            <HourlyTemperature
+            data={forecastQuery.data}/>
+         
+          </div>
+
+          <div>
+          <WeatherForecast data={forecastQuery.data} />
+            {/* forecast */}
+          </div>
+        </div>
+    </div>
   )
+  
 }
 
 export default CityPage;
